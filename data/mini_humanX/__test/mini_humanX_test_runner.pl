66ae52bab762d5547066f63c1f1e504404fbf9ca
Brian Haas <brianjohnhaas@gmail.com>
2017-09-07 15:29:13 -0400

    add

---
+++ data/mini_humanX/__test/mini_humanX_test_runner.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use FindBin;
+use lib("$FindBin::Bin/../../__lib");
+use Pipeliner;
+
+main: {
+
+    my $pipeliner = new Pipeliner(-verbose => 2);
+
+    ## run hisat
+    $pipeliner->add_commands(new Command("hisat2_extract_splice_sites.py minigenome.gtf > minigenome.gtf.ss", "hisat_ss.ok"));
+
+    $pipeliner->add_commands(new Command("hisat2_extract_exons.py  minigenome.gtf > minigenome.gtf.exons", "hisat_exons.ok"));
+
+    $pipeliner->add_commands(new Command("hisat2-build --exon minigenome.gtf.exons --ss minigenome.gtf.ss minigenome.fa minigenome.fa",
+                                         "hisat_build.ok"));
+
+    $pipeliner->add_commands(new Command("hisat2 --dta -x minigenome.fa -1 reads_1.fq.gz -2 reads_2.fq.gz | samtools view -Sb | samtools sort -o alignments.hisat2.bam", "hisat_align.ok"));
+
+    ## run stringtie to reconstruct transcripts
+    $pipeliner->add_commands(new Command("stringtie -o stringtie.gtf alignments.hisat2.bam", "stringtie.ok"));
+
+
+    $pipeliner->run();
+
+    exit(0);
+}
+
+
+    
+    

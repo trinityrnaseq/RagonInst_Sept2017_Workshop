db3395774efb01b5c6e044e4eaf8d66667886c16
Brian Haas <brianjohnhaas@gmail.com>
2017-09-08 13:27:51 -0400

    add

--- data/mini_humanX/__test/mini_humanX_test_runner.pl
+++ data/mini_humanX/__test/mini_humanX_test_runner.pl
@@ -19,11 +19,14 @@ main: {
     $pipeliner->add_commands(new Command("hisat2-build --exon minigenome.gtf.exons --ss minigenome.gtf.ss minigenome.fa minigenome.fa",
                                          "hisat_build.ok"));
 
-    $pipeliner->add_commands(new Command("hisat2 --dta -x minigenome.fa -1 reads_1.fq.gz -2 reads_2.fq.gz | samtools view -Sb | samtools sort -o alignments.hisat2.bam", "hisat_align.ok"));
+    $pipeliner->add_commands(new Command("hisat2 --dta -x minigenome.fa --max-intronlen 5000 -1 reads_1.fq.gz -2 reads_2.fq.gz | samtools view -Sb | samtools sort -o alignments.hisat2.bam", "hisat_align.ok"));
 
     ## run stringtie to reconstruct transcripts
     $pipeliner->add_commands(new Command("stringtie -o stringtie.gtf alignments.hisat2.bam", "stringtie.ok"));
 
+    
+    $pipeliner->add_commands(new Command("Trinity --genome_guided_bam alignments.hisat2.bam --max_memory 1G --genome_guided_max_intron 5000 --CPU 2", "trinityGG.ok"));
+    
 
     $pipeliner->run();
 
